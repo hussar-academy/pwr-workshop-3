6c45572869a2d35dd606167e73a4dcf255f73991
hodak <kaczor555@gmail.com>
2014-11-08 16:15:14 +0100

    Add api/v1 namespace, return 404 on wrong api endpoint – don't redirect to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+    end
+  end
+
+  get 'api' => proc { [404, {}, ['Invalid API endpoint']] }
+  get 'api/*path' => proc { [404, {}, ['Invalid API endpoint']] }
+
   root 'home#index'
   get '*path', to: 'home#index'
 end
