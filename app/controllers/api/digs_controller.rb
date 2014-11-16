bac7e1422bbd4429360d554c8fbd38d3882f9049
Jakub Cieslar <cieslar.jakub@gmail.com>
2014-11-17 00:00:52 +0100

    CRUD for digs - endpoints

--- app/controllers/api/digs_controller.rb
+++ app/controllers/api/digs_controller.rb
@@ -5,4 +5,37 @@ class Api::DigsController < ApplicationController
       format.json { render json: digs }
     end
   end
+
+  def update
+    dig = Dig.find(params[:id])
+    dig.update(dig_params)
+
+    respond_to do |format|
+      format.json { render json: dig }
+    end
+  end
+
+  def create
+    dig = current_user.digs.new(dig_params)
+    dig.save!
+
+    respond_to do |format|
+      format.json { render json: dig }
+    end
+  end
+
+  def destroy
+    dig = Dig.find(params[:id])
+    dig.destroy
+
+    respond_to do |format|
+      format.json { render json: { resp: 'success' } }
+    end
+
+  end
+
+  private
+    def dig_params
+      params.require(:dig).permit(:title, :body, :owner_id)
+    end
 end
